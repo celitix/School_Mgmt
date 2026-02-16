7574df65ae7301ddb218bf90c7823d90a8b35ad3
Arihant Jain <arihantj916@gmail.com>
2026-02-16 15:04:49 +0530

    feat: create service and dto class to handle teacher creations

--- src/teachers/dto/create-teacher.dto.ts
+++ src/teachers/dto/create-teacher.dto.ts
@@ -1,1 +1,62 @@
-export class CreateTeacherDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsInt, IsOptional, IsString } from 'class-validator';
+
+export class CreateTeacherDto {
+  @ApiProperty({
+    example: 'Arihant',
+    description: 'Student Name',
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    example: '96XXXXXXXX',
+    description: 'phone',
+  })
+  @IsString()
+  phone: string;
+
+  @ApiProperty({
+    example: 'abc@gmail',
+    description: 'registered email',
+    required: false,
+  })
+  @IsString()
+  email: string;
+
+  @ApiProperty({
+    example: 'Flat no 123, Jaipur',
+    description: 'registered address. format address 1, address 2, address 3',
+    required: false,
+  })
+  @IsString()
+  address: string;
+
+  @ApiProperty({
+    example: 'ENR12345',
+    description: 'Unique enrollment number of employee',
+  })
+  @IsString()
+  enrollmentNo: string;
+
+  @ApiProperty({
+    example: 'B.Tech Computer Science',
+    description: 'Educational qualification of employee',
+  })
+  @IsString()
+  qualification: string;
+
+  @ApiProperty({
+    example: '3 years in backend development',
+    description: 'Total work experience',
+  })
+  @IsString()
+  exoperience: string;
+
+  @ApiProperty({
+    example: '2026-02-16',
+    description: 'Joining date in ISO format (YYYY-MM-DD)',
+  })
+  @IsDateString()
+  joiningDate: Date;
+}
--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -1,26 +1,89 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTeacherDto } from './dto/create-teacher.dto';
 import { UpdateTeacherDto } from './dto/update-teacher.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TeachersService {
-  create(createTeacherDto: CreateTeacherDto) {
-    return 'This action adds a new teacher';
+  constructor(private readonly prisma: PrismaService) {}
+  async create(data: CreateTeacherDto) {
+    const { name, phone, address, email, ...rest } = data;
+    return await this.prisma.$transaction(async (tx) => {
+      const user = await tx.users.create({
+        data: {
+          name,
+          phone,
+          address,
+          email,
+          roleId: 3,
+        },
+      });
+
+      return await tx.teachers.create({
+        data: {
+          ...rest,
+          userId: user.id,
+        },
+      });
+    });
   }
 
-  findAll() {
-    return `This action returns all teachers`;
+  async isTeacherExist(phone: string) {
+    return await this.prisma.users.findUnique({ where: { phone } });
+  }
+  async isTeacherExistById(id: string) {
+    return await this.prisma.teachers.findUnique({
+      where: { id },
+      include: {
+        user: true,
+      },
+    });
+  }
+  async findAll(page = 1, limit = 10) {
+    const skip = (page - 1) * limit;
+    const [teachers, total] = await Promise.all([
+      this.prisma.teachers.findMany({
+        include: {
+          user: true,
+        },
+        skip,
+        take: limit,
+        orderBy: {
+          createdAt: 'desc', // optional but recommended
+        },
+      }),
+      this.prisma.teachers.count(),
+    ]);
+
+    return {
+      data: teachers,
+      meta: {
+        total,
+        page,
+        limit,
+        totalPages: Math.ceil(total / limit),
+        hasNextPage: page * limit < total,
+        hasPreviousPage: page > 1,
+      },
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} teacher`;
+  async findOne(id: string) {
+    return await this.prisma.teachers.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        user: true,
+      },
+    });
   }
 
-  update(id: number, updateTeacherDto: UpdateTeacherDto) {
+  async update(id: string, updateTeacherDto: UpdateTeacherDto) {
     return `This action updates a #${id} teacher`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} teacher`;
+  async remove(id: string) {
+    return await this.prisma.users.delete({ where: { id } });
   }
 }
