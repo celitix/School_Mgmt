1be37452df8ee110d6989f88572c1f7ed4af4712
Arihant Jain <arihantj916@gmail.com>
2026-02-19 11:30:39 +0530

    feat: create dto class for create salary structure dto

---
+++ src/teachers/dto/create-salary-structure.dto.ts
@@ -0,0 +1,73 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDateString, IsInt, IsOptional, IsString } from 'class-validator';
+
+export class CreateSalaryStructureDto {
+  @ApiProperty({
+    example: '2024-04-01T00:00:00.000Z',
+    description: 'Effective From',
+  })
+  @IsDateString()
+  effectiveFrom: string;
+
+  @ApiProperty({
+    example: '2024-04-01T00:00:00.000Z',
+    description: 'Effective To',
+    required: false,
+  })
+  @IsDateString()
+  effectiveTo?: string;
+
+  @ApiProperty({
+    example: '50000',
+    description: 'Basic Salary',
+  })
+  @IsInt()
+  basicSalary: number;
+
+  @ApiProperty({
+    example: '5000',
+    description: 'House Rent Allowance',
+    required: false,
+  })
+  @IsInt()
+  hra: number;
+
+  @ApiProperty({
+    example: '5050',
+    description: 'Allowance',
+    required: false,
+  })
+  @IsInt()
+  allowance: number;
+
+  @ApiProperty({
+    example: '500',
+    description: 'Provident Fund',
+    required: false,
+  })
+  @IsInt()
+  pf: number;
+
+  @ApiProperty({
+    example: '100',
+    description: "Employees' State Insurance",
+    required: false,
+  })
+  @IsInt()
+  esi: number;
+
+  @ApiProperty({
+    example: '50000',
+    description: 'Net Salary',
+  })
+  @IsInt()
+  netSalary: number;
+
+  @ApiProperty({
+    example: '100',
+    description: 'Professional Tax',
+    required: false,
+  })
+  @IsInt()
+  professionalTax: number;
+}
